21b119d8302111921a42ee6905536cdd24d1b34d
Anistea <43377371+Anistea@users.noreply.github.com>
2020-02-03 15:08:28 +0100

    BDD sql

---
+++ Personnel/Documentation/personnel.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  lun. 03 fév. 2020 à 14:03
+-- Version du serveur :  5.7.19
+-- Version de PHP :  5.6.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `personnel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `appartenir`
+--
+
+DROP TABLE IF EXISTS `appartenir`;
+CREATE TABLE IF NOT EXISTS `appartenir` (
+  `idEmploye` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`idEmploye`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `employe`
+--
+
+DROP TABLE IF EXISTS `employe`;
+CREATE TABLE IF NOT EXISTS `employe` (
+  `idEmploye` int(11) NOT NULL AUTO_INCREMENT,
+  `lvl` int(11) NOT NULL DEFAULT '0',
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `mail` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `dateFin` date NOT NULL,
+  `dateDebut` date NOT NULL,
+  PRIMARY KEY (`idEmploye`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ligue`
+--
+
+DROP TABLE IF EXISTS `ligue`;
+CREATE TABLE IF NOT EXISTS `ligue` (
+  `idLigue` int(11) NOT NULL AUTO_INCREMENT,
+  `nomLigue` varchar(255) NOT NULL,
+  PRIMARY KEY (`idLigue`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
